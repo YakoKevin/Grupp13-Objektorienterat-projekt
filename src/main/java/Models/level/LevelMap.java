f2279f2abc0f3e5cccd4054085287330f10cb3a4
Julia <juliakaufmann@live.se>
2022-09-19 14:04:38 +0200

    Add level package with simple level classes

---
+++ src/main/java/Models/level/LevelMap.java
@@ -0,0 +1,4 @@
+package Models.level;
+
+public class LevelMap {
+}
---
+++ src/main/java/Models/level/LevelMapFunction.java
@@ -0,0 +1,11 @@
+package Models.level;
+
+import java.util.function.Function;
+
+public class LevelMapFunction implements Function<Integer, LevelMap> {
+
+    @Override
+    public LevelMap apply(Integer integer) {
+        return null;
+    }
+}
