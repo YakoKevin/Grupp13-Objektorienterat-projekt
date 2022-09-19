0a4ae15b3d1a0cf3076edaab98aab685ad792676
Kevin Yako <yakokevin@gmail.com>
2022-09-19 16:55:36 +0200

    Added movement, and an X and Y coordinate variable, and moved code to View

--- src/main/java/GameApp.java
+++ src/main/java/GameApp.java
@@ -1,17 +1,10 @@
-import com.almasb.fxgl.app.GameApplication;
-import com.almasb.fxgl.app.GameSettings;
+import Views.GameView;
+
 
 // TEMPORÄRT KLASS NAMN
-public class GameApp extends GameApplication{
+public class GameApp {
 
-    @Override
-    protected void initSettings(GameSettings gameSettings) {
-        gameSettings.setWidth(800);
-        gameSettings.setHeight(800);
-        gameSettings.setTitle("DungeonCrawler");
-        gameSettings.setVersion("1.0");
-    }
     public static void main(String[] args) {
-        launch(args);
+        GameView.start(args);
     }
 }
