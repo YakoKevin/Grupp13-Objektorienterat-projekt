399cfc52ae91928fa3f2f013c11550bb39df5914
MhdJamal <jamalbasal35@gmail.com>
2022-09-26 00:10:22 +0200

    Added text with color to Start menu

--- src/main/java/Views/MainMenu.java
+++ src/main/java/Views/MainMenu.java
@@ -7,8 +7,13 @@ import java.awt.*;
 
 public class MainMenu extends GameState {
 
+    private String[] optionsMenu;
+    private int selected;
+
     public MainMenu(GameStateManager manager) {
         super(manager);
+        this.optionsMenu = new String[] {"START_GAME"};
+        this.selected = 0;
     }
 
     @Override
@@ -18,8 +23,14 @@ public class MainMenu extends GameState {
 
     @Override
     protected void render(Graphics graphics) {
+        graphics.setColor(new Color(30, 30, 70));
         graphics.fillRect(0, 0, WindowManager.WIDTH, WindowManager.HEIGHT);
 
+        for(int i=0;i<this.optionsMenu.length;i++) {
+            if(i==this.selected) graphics.setColor(Color.GREEN);
+            else graphics.setColor(Color.WHITE);
+            graphics.drawString(this.optionsMenu[i], 20, 50 + i * 40);
+        }
     }
 
     @Override
