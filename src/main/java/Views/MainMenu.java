4aad310c1a15392b1bf5da8bbc3634cfb8e81863
MhdJamal <jamalbasal35@gmail.com>
2022-09-25 20:33:06 +0200

    Added MainMenu class start menu

---
+++ src/main/java/Views/MainMenu.java
@@ -0,0 +1,38 @@
+package Views;
+
+import Controllers.gamestates.GameState;
+import Controllers.gamestates.GameStateManager;
+
+import java.awt.*;
+
+public class MainMenu extends GameState {
+
+    protected MainMenu(GameStateManager manager) {
+        super(manager);
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void loop() {
+
+    }
+
+    @Override
+    protected void render(Graphics graphics) {
+
+    }
+
+    @Override
+    protected void keyPressed(int keyCode) {
+
+    }
+
+    @Override
+    protected void keyReleased(int keyCode) {
+
+    }
+}
