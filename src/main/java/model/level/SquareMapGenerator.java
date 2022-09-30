4eb48f6df65edc92c40d5c9e45a8d05f0b1900c6
Julia <juliakaufmann@live.se>
2022-09-30 22:29:57 +0200

    Make map generator class not instantiable

--- src/main/java/model/level/SquareMapGenerator.java
+++ src/main/java/model/level/SquareMapGenerator.java
@@ -10,7 +10,8 @@ import java.util.concurrent.ThreadLocalRandom;
  * the border size of the map. Generates a LevelMap with a random amount of nodes and edges connecting those. There is
  * always a path from a starting coordinate to an end coordinate.
  */
-public class SquareMapGenerator {
+public enum SquareMapGenerator {
+    ;
 
     public static LevelMap generate(int size) {
         if(size < 4)
