3b2ee49d8a60e7ac45a436bb31e72e2d1d4d4096
Kevin Yako <yakokevin@gmail.com>
2022-10-18 17:53:55 +0200

    Removed commented code, added some javadoc to important view classes

--- src/main/java/view/FPSUpdater.java
+++ src/main/java/view/FPSUpdater.java
@@ -56,7 +56,6 @@ public class FPSUpdater implements Runnable {
             previousTime = currentTime;
 
             if (deltaU >= 1){
-                this.gamePanel.update();
                 updates++;
                 deltaU--;
             }
@@ -67,68 +66,10 @@ public class FPSUpdater implements Runnable {
                 deltaF--;
             }
 
-            //if (now - lastFrame >= timePerFrame){
-            //    gamePanel.repaint();
-            //    lastFrame = System.nanoTime();
-            //    fps++;
-            //}
-
             if (System.currentTimeMillis() - lastChecked >= 1000){
                 lastChecked = System.currentTimeMillis();
-//                System.out.println("FPS: " + fps + " | UPS: " + updates);
-
                 fps = 0;
                 updates = 0;
-                //TODO: Must move and redo! Can not lay in view-class. Do more dynamically, use objects (our enemies)!
-/*
-                float enemyX = this.gamePanel.animationEnemy.entity.getX();
-                float enemyY = this.gamePanel.animationEnemy.entity.getY();
-
-                float playerX = this.gamePanel.animation.entity.getX();
-                float playerY = this.gamePanel.animation.entity.getY();
-
-//                System.out.println("--" + Math.abs(playerX - enemyX));
-//                System.out.println("--" + Math.abs(playerY - enemyY));
-
-                int enemyMoveDistance = 55;
-                boolean isNear = false;
-                if(Math.abs(playerX - enemyX) < 150 && Math.abs(playerY - enemyY) < 150) {
-                    isNear = true;
-                }
-
-                if(isNear) {
-                    if(Math.abs(playerX - enemyX) > Math.abs(playerY - enemyY)) {
-                        if(enemyX > playerX) {
-                            this.gamePanel.animationEnemy.entity.setX(this.gamePanel.animationEnemy.entity.getX()-enemyMoveDistance);
-                        }else {
-                            this.gamePanel.animationEnemy.entity.setX(this.gamePanel.animationEnemy.entity.getX()+enemyMoveDistance);
-                        }
-                    }else {
-                        if(enemyY > playerY) {
-                            this.gamePanel.animationEnemy.entity.setY(this.gamePanel.animationEnemy.entity.getY()-enemyMoveDistance);
-                        }else {
-                            this.gamePanel.animationEnemy.entity.setY(this.gamePanel.animationEnemy.entity.getY()+enemyMoveDistance);
-                        }
-                    }
-                }else {
-                    // move random
-                    int random = new Random().nextInt(4);
-
-                    if(random == 0) {
-                        this.gamePanel.animationEnemy.entity.setX(this.gamePanel.animationEnemy.entity.getX()+enemyMoveDistance);
-                    }else if(random == 1) {
-                        this.gamePanel.animationEnemy.entity.setY(this.gamePanel.animationEnemy.entity.getY()+enemyMoveDistance);
-                    }else if(random == 2) {
-                        this.gamePanel.animationEnemy.entity.setX(this.gamePanel.animationEnemy.entity.getX()-enemyMoveDistance);
-                    }else if(random == 3) {
-                        this.gamePanel.animationEnemy.entity.setY(this.gamePanel.animationEnemy.entity.getY()-enemyMoveDistance);
-                    }
-                }
-
-                if(Math.abs(this.gamePanel.animationEnemy.entity.getX() - this.gamePanel.animation.entity.getX()) < 20 && Math.abs(this.gamePanel.animationEnemy.entity.getY() - this.gamePanel.animation.entity.getY()) < 20) {
-                    this.gamePanel.player.healthPoints = this.gamePanel.player.healthPoints - 10;
-                }
-*/
             }
 
         }
