69609e8fe997db93ca273ed607e3b4596fbf2e86
MhdJamal <jamalbasal35@gmail.com>
2022-10-18 23:01:33 +0200

    Added Enemy Movement

--- src/main/java/entity/Enemy.java
+++ src/main/java/entity/Enemy.java
@@ -5,6 +5,8 @@ import model.Movement;
 import utilz.Coordinate;
 
 import java.awt.*;
+import java.time.LocalTime;
+import java.util.Random;
 
 public abstract class Enemy extends Living implements Hostile {
     private Brain brain;
@@ -34,31 +36,59 @@ public abstract class Enemy extends Living implements Hostile {
         private float friendlyLastSeenY = Float.MIN_VALUE;
         private final float sightRange;
         private boolean readyToAttack;
+        private LocalTime lastRenderTime = LocalTime.now();
 
         public Brain(float sightRange){
             this.sightRange = sightRange;
         }
 
         public void think() { //går kanske att ha ett mer beskrivande namn :)
-            if(Math.abs(friendlyLastSeenX - getX()) > sightRange &&
-                    Math.abs(friendlyLastSeenY - getY()) > sightRange){
+            if(Math.abs(friendlyLastSeenX - finePositionX) < 150 && Math.abs(friendlyLastSeenY - finePositionY) < 150) {
+                moveTowardsFriendly();
+            }else {
                 moveRandomly();
-            } else if(friendly != null){
-                if(Math.abs(friendlyLastSeenX - getX()) > attack.getAttackRange() &&
-                        Math.abs(friendlyLastSeenY - getY()) > attack.getAttackRange()){
-                    moveTowardsFriendly();
-                }else if(readyToAttack)
-                    attackFriendly();
             }
-            readyToAttack = attack.coolDown();
+//        	System.out.println(friendlyLastSeenX + " : " + finePositionX + " : " + finePositionX + velX);
         }
 
         private void moveRandomly(){
-            //System.out.println("MOVE RANDOMLY");
+            LocalTime now = LocalTime.now();
+            if(!(now.isAfter(lastRenderTime) && now.minusSeconds(1).isBefore(lastRenderTime))) {
+                lastRenderTime = now;
+                int enemyMoveDistance = 15;
+                int random = new Random().nextInt(4);
+                if(random == 0) {
+                    finePositionX += enemyMoveDistance;
+                }else if(random == 1) {
+                    finePositionY += enemyMoveDistance;
+                }else if(random == 2) {
+                    finePositionX -= enemyMoveDistance;
+                }else if(random == 3) {
+                    finePositionY -= enemyMoveDistance;
+                }
+
+            }
         }
 
         private void moveTowardsFriendly(){
-            //System.out.println("MOVE TOWARDS FRIENDLY");
+            LocalTime now = LocalTime.now();
+            if(!(now.isAfter(lastRenderTime) && now.minusSeconds(1).isBefore(lastRenderTime))) {
+                lastRenderTime = now;
+                int enemyMoveDistance = 15;
+                if(Math.abs(friendlyLastSeenX - finePositionX) > Math.abs(friendlyLastSeenY - finePositionY)) {
+                    if(finePositionX > friendlyLastSeenX) {
+                        finePositionX -= enemyMoveDistance;
+                    }else {
+                        finePositionX += enemyMoveDistance;
+                    }
+                }else {
+                    if(finePositionY > friendlyLastSeenY) {
+                        finePositionY -= enemyMoveDistance;
+                    }else {
+                        finePositionY += enemyMoveDistance;
+                    }
+                }
+            }
         }
 
         private void attackFriendly(){
