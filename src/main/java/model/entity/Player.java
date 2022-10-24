f5d20621dcde9c8f950717318dbc56b5e49587d3
HannesRL <hrlarsson@yahoo.se>
2022-10-24 13:39:33 +0200

    Råkade ändra namn på checkEntityMovement utan att ändra på refactor alla namn

--- src/main/java/model/entity/Player.java
+++ src/main/java/model/entity/Player.java
@@ -57,7 +57,7 @@ public class Player extends Living implements Friendly {
 
     @Override
     public void tickExtra() {
-        checkEntityMovement();
+        checkLivingMovement();
         checkKeys();
         checkCoins();
         if(!takenHeart){
