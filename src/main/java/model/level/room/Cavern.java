6653339b3c3874a9bb15b4b8f61a613e76426753
HannesRL <hrlarsson@yahoo.se>
2022-10-24 20:16:40 +0200

    Lagt till java-doc för room-klasser

--- src/main/java/model/level/room/Cavern.java
+++ src/main/java/model/level/room/Cavern.java
@@ -16,7 +16,10 @@ import java.util.concurrent.ThreadLocalRandom;
  * @see EnemyFactory
  */
 public class Cavern extends Room {
-
+    /**
+     * Setting up cavern with room objects.
+     * @param doors
+     */
     public Cavern(ArrayList<Door> doors) {
         super(doors, new EnemyFactory());
         createWalls();
@@ -27,6 +30,9 @@ public class Cavern extends Room {
         addHeart();
     }
 
+    /**
+     * Creating the room's walls
+     */
     private void createWalls() {
         int y = 0;
         ArrayList<Coordinate> doorCoordinates = new ArrayList<>();
@@ -47,18 +53,34 @@ public class Cavern extends Room {
         addVerticalWallIfPossible(doorCoordinates, x);
     }
 
+    /**
+     * Adding wall on the vertical side of the room.
+     * @param doorCoordinates
+     * @param x
+     */
     private void addVerticalWallIfPossible(ArrayList<Coordinate> doorCoordinates, int x) {
         for (int y = 0; y < HEIGHT; y++){
             addWallIfPossible(doorCoordinates, x, y);
         }
     }
 
+    /**
+     * Adding wall on the horizontal side of the room.
+     * @param y
+     * @param doorCoordinates
+     */
     private void addHorizontalWallIfPossible(int y, ArrayList<Coordinate> doorCoordinates) {
         for (int x = 0; x < WIDTH; x++) {
             addWallIfPossible(doorCoordinates, x, y);
         }
     }
 
+    /**
+     * Only adding wall if there is not a door at that coordinate.
+     * @param doorCoordinates
+     * @param x
+     * @param y
+     */
     private void addWallIfPossible(ArrayList<Coordinate> doorCoordinates, int x, int y) {
         boolean intersectingDoor = false;
         Coordinate wallCoordinate = new Coordinate(x, y);
@@ -73,6 +95,10 @@ public class Cavern extends Room {
         }
     }
 
+    /**
+     * Adding enemies randomly to the room.
+     * @param enemyFactory
+     */
     private void addEnemies(EnemyFactory enemyFactory) {
         for (int i = 0; i <= Constants.getRandomEnemiesAmount(); i++) {
             int x = ThreadLocalRandom.current().nextInt(1, GameConstants.RoomMapSizes.WIDTH.getSize());
@@ -83,6 +109,9 @@ public class Cavern extends Room {
         }
     }
 
+    /**
+     * Adding obstacles randomly to the room
+     */
     private void addObstacles() {
         for (int i = 0; i < Constants.getRandomObstaclesAmount(); i++) {
             Coordinate randomCoordinate = Coordinate.randomCoordinate(1, 1, WIDTH - 2, HEIGHT - 2);
@@ -90,6 +119,9 @@ public class Cavern extends Room {
         }
     }
 
+    /**
+     * Adding keys randomly to the room
+     */
     private void addKeys() {
         for (int i = 0; i < Constants.getRandomKeysAmount(); i++) {
             Coordinate randomCoordinate = Coordinate.randomCoordinate(1, 1, WIDTH - 2, HEIGHT - 2);
@@ -97,6 +129,9 @@ public class Cavern extends Room {
         }
     }
 
+    /**
+     * Adding coins randomly to the room
+     */
     private void addCoins() {
         for (int i = 0; i < Constants.getRandomKeysAmount(); i++) {
             Coordinate randomCoordinate = Coordinate.randomCoordinate(1, 1, WIDTH - 2, HEIGHT - 2);
@@ -104,11 +139,16 @@ public class Cavern extends Room {
         }
     }
 
+    /**
+     * Adding heart coordinate somewhere random in the room.
+     */
     private void addHeart() {
         heartCoordinate = Coordinate.randomCoordinate(1, 1, WIDTH - 2, HEIGHT - 2);
     }
 
-
+    /**
+     * Constants for minimum and maximum amount of objects in the room.
+     */
     private enum Constants {
         MIN_NUMBER_OF_ENEMIES(0),
         MAX_NUMBER_OF_ENEMIES(4),
@@ -120,22 +160,39 @@ public class Cavern extends Room {
         MAX_NUMBER_OF_COINS(6);
         int amount;
 
+        /**
+         * Setting up amount
+         * @param amount
+         */
         Constants(int amount) {
             this.amount = amount;
         }
 
+        /**
+         * Getting random enemies amount
+         * @return
+         */
         static int getRandomEnemiesAmount() {
             return ThreadLocalRandom.current().nextInt(Constants.MIN_NUMBER_OF_ENEMIES.amount, Constants.MAX_NUMBER_OF_ENEMIES.amount + 1);
         }
-
+        /**
+         * Getting random obstacles amount
+         * @return
+         */
         static int getRandomObstaclesAmount() {
             return ThreadLocalRandom.current().nextInt(Constants.MIN_NUMBER_OF_OBSTACLES.amount, Constants.MAX_NUMBER_OF_OBSTACLES.amount + 1);
         }
-
+        /**
+         * Getting random keys amount
+         * @return
+         */
         static int getRandomKeysAmount() {
             return ThreadLocalRandom.current().nextInt(Constants.MIN_NUMBER_OF_KEYS.amount, Constants.MAX_NUMBER_OF_KEYS.amount + 1);
         }
-
+        /**
+         * Getting random keys amount
+         * @return
+         */
         static int getRandomCoinsAmount() {
             return ThreadLocalRandom.current().nextInt(Constants.MIN_NUMBER_OF_COINS.amount, Constants.MAX_NUMBER_OF_COINS.amount + 1);
         }
--- src/main/java/model/level/room/Room.java
+++ src/main/java/model/level/room/Room.java
@@ -29,7 +29,11 @@ public abstract class Room{
     protected EnemyFactory enemyFactory;
     private final int DOOR_DISTANCE = 2;
 
-
+    /**
+     * Setting up room, with correct size, and doors and enemies
+     * @param doors
+     * @param enemyFactory
+     */
     public Room(ArrayList<Door> doors, EnemyFactory enemyFactory) {
         this.enemyFactory = enemyFactory;
         this.HEIGHT = RoomMapSizes.HEIGHT.getSize();
@@ -37,6 +41,11 @@ public abstract class Room{
         addDoors(doors);
     }
 
+    /**
+     * Checking if a coordinate is in a wall.
+     * @param coordinate
+     * @return
+     */
     public boolean isCoordinateInWall(Coordinate coordinate) {
         for (Coordinate coordinateWall : wallCoordinates) {
             if (coordinate.equals(coordinateWall))
@@ -45,6 +54,11 @@ public abstract class Room{
         return false;
     }
 
+    /**
+     * Checking if a coordinate is in an obstacle
+     * @param coordinate
+     * @return
+     */
     public boolean isCoordinateInObstacle(Coordinate coordinate) {
         for (Coordinate coordinateObs : obstaclesCoordinates) {
             if (coordinate.equals(coordinateObs))
@@ -53,10 +67,21 @@ public abstract class Room{
         return false;
     }
 
+    /**
+     * Checking if a coordinate is in either a wall or obstacle
+     * @param coordinate
+     * @return
+     */
     public boolean isCoordinateInWallOrObstacle(Coordinate coordinate) {
         return isCoordinateInObstacle(coordinate) || isCoordinateInWall(coordinate);
     }
 
+    /**
+     * Checking if a coordinate is in a door
+     * @param x
+     * @param y
+     * @return
+     */
     public boolean isCoordinateInDoor(float x, float y){
         for(Door door : doors){
             float scale = GameScalingFactors.TILE_SCALE_FACTOR.getSize();
@@ -67,14 +92,26 @@ public abstract class Room{
         return false;
     }
 
+    /**
+     * Get x of coordinate
+     * @return
+     */
     public int getX(){
         return coordinate.getX();
     }
 
+    /**
+     * Get y of coordinate
+     * @return
+     */
     public int getY(){
         return coordinate.getY();
     }
 
+    /**
+     * Getting the room as a matrix.
+     * @return
+     */
     public int[][] getRoomAsMatrix() {
         int[][] matrix = new int[WIDTH][HEIGHT];
 
@@ -100,52 +137,103 @@ public abstract class Room{
         return matrix;
     }
 
+    /**
+     * Setting coordinate
+     * @param coordinate
+     */
     public void setCoordinate(Coordinate coordinate){
         this.coordinate = coordinate;
     }
 
+    /**
+     * Getting wall coordinates
+     * @return
+     */
     public ArrayList<Coordinate> getWalls(){
         return new ArrayList<>(wallCoordinates);
     }
 
+    /**
+     * Getting obstacle coordinates
+     * @return
+     */
     public ArrayList<Coordinate> getObstacles(){
         return new ArrayList<>(obstaclesCoordinates);
     }
 
+    /**
+     * Getting key coordinates
+     * @return
+     */
     public ArrayList<Coordinate> getKeys(){
         return keysCoordinates;
     }
 
+    /**
+     * Getting coin coordinates
+     * @return
+     */
     public ArrayList<Coordinate> getCoins(){
         return coinsCoordinates;
     }
 
+    /**
+     * Get coordinate of heart
+     * @return
+     */
     public Coordinate getHeart(){
         return heartCoordinate;
     }
+
+    /**
+     * Get door coordinates
+     * @return
+     */
     public ArrayList<Door> getDoors(){
         return new ArrayList<>(doors);
     }
 
+    /**
+     * Get enemy coordinates
+     * @return
+     */
     public ArrayList<Enemy> getEnemies(){
         return new ArrayList<>(enemies);
     }
 
+    /**
+     * Giving the player the hostiles (enemies).
+     * @param player
+     */
     public void givePlayerHostiles(Player player){
         ArrayList<Hostile> hostiles = new ArrayList<>(enemies);
         player.addHostilesList(hostiles);
     }
+
+    /**
+     * Giving the player its score.
+     * @param player
+     */
     public void setPlayerTotalScore(Player player){
         player.setScoreCount(player.getRoomScoreCount()+player.getScoreCount());
     }
 
-
+    /**
+     *
+     * @param friendly
+     */
     public void giveEnemiesFriendly(Friendly friendly){
         for(Enemy enemy : enemies){
             enemy.addFriendly(friendly);
         }
     }
 
+    /**
+     * Getting the door located closest to coordinate
+     * @param position
+     * @return
+     * @throws Exception
+     */
     public Door getClosestDoor(Coordinate position) throws Exception {
         for(Door door : doors){
             if(Math.abs(position.getX() - door.coordinate.getX()) < DOOR_DISTANCE && Math.abs(position.getY() - door.coordinate.getY()) < DOOR_DISTANCE){
@@ -155,10 +243,17 @@ public abstract class Room{
         throw new Exception("No door found but request of a door close by was made");
     }
 
+    /**
+     * Adding doors
+     * @param doors
+     */
     private void addDoors(ArrayList<Door> doors) {
         this.doors = doors;
     }
 
+    /**
+     * Removing the enemies from enemy list.
+     */
     public void removeEnemies(){
         enemies.clear();
     }
